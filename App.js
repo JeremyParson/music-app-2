48ca39a8f1d717296fd59a38826dfdfa0f85c815
Jeremy Parson <jsbparson@gmail.com>
2022-03-24 17:57:05 -0400

    complete

--- App.js
+++ App.js
@@ -1,37 +1,55 @@
-import './App.css';
-import SearchBar from './components/SearchBar';
-import Gallery from './components/Gallery';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+import Gallery from "./components/Gallery";
+import { Fragment, useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AlbumView from "./views/AlbumView";
+import ArtistView from "./views/ArtistView";
 
 function App() {
-  let [search, setSearch] = useState('')
-  let [message, setMessage] = useState('Search for Music!')
-  let [data, setData] = useState([])
+  let [search, setSearch] = useState("");
+  let [message, setMessage] = useState("Search for Music!");
+  let [data, setData] = useState([]);
 
   useEffect(() => {
-    if (search == '') return
+    if (search == "") return;
     const fetchData = async () => {
-      document.title = `${search} Music`
-      const query = search.split(' ').join('+')
-      const response = await fetch(`https://itunes.apple.com/search?term=${query}`)
-      const json = await response.json()
-      console.log(json)
+      document.title = `${search} Music`;
+      const query = search.split(" ").join("+");
+      const response = await fetch(
+        `https://itunes.apple.com/search?term=${query}`
+      );
+      const json = await response.json();
+      console.log(json);
       if (json.results.length > 0) {
-        setData(json.results)
-        setMessage('Songs found')
+        setData(json.results);
+        setMessage("Songs found");
       } else {
-        setMessage('No songs found')
+        setMessage("No songs found");
       }
-        
-    }
-    fetchData()
-  }, [search])
+    };
+    fetchData();
+  }, [search]);
 
   return (
     <div className="App">
-      <SearchBar setSearch={setSearch}/>
       {message}
-      {data.length ? <Gallery data={data}/> : <div>No Tracks</div>}
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Fragment>
+                <SearchBar setSearch={setSearch} />
+
+                {data.length ? <Gallery data={data} /> : <div>No Tracks</div>}
+              </Fragment>
+            }
+          />
+          <Route path="/album/:id"  element={<AlbumView />}/>
+          <Route path="/artist/:id"  element={<ArtistView />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- components/GalleryItem.jsx
+++ components/GalleryItem.jsx
@@ -1,35 +1,46 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-function GalleryItem (props) {
-    let [view, setView] = useState(false)
-    
-    const simpleView = (
-        <div>
-            <div>
-                <img src={props.song.artworkUrl100} alt={`Image of ${props.song.trackName} track cover art.`}/>
-                <h3>{props.song.trackName}</h3>
-                <h4>{props.song.collectionName}</h4>
-            </div>
-        </div>
-    )
+function GalleryItem(props) {
+  let [view, setView] = useState(false);
 
-    const detailedView = (
-        <div>
-            <div>
-                <img src={props.song.artworkUrl100} alt={`Image of ${props.song.trackName} track cover art.`}/>
-                <h3>{props.song.trackName}</h3>
-                <h4>{props.song.collectionName}</h4>
-                <h4>{props.song.primaryGenreName}</h4>
-                <h4>{props.song.releaseDate}</h4>
-            </div>
-        </div>
-    )
+  const simpleView = (
+    <div>
+      <img
+        src={props.song.artworkUrl100}
+        alt={`Image of ${props.song.trackName} track cover art.`}
+      />
+      <h3>{props.song.trackName}</h3>
+      <h4>{props.song.collectionName}</h4>
+    </div>
+  );
 
-    return (
-    <div onClick={() => setView(!view)} style={{'display': 'inline-block'}}>
-        <p>{props.title}</p>
-        {view ? detailedView : simpleView}
-    </div>)
+  const detailedView = (
+    <div>
+      <img
+        src={props.song.artworkUrl100}
+        alt={`Image of ${props.song.trackName} track cover art.`}
+      />
+      <h2>{props.song.trackName}</h2>
+      <h3>
+        <Link to={`/artist/${props.song.artistId}`}>{props.song.artistId}</Link>
+      </h3>
+      <h3>
+        <Link to={`/artist/${props.song.collectionId}`}>
+          {props.song.collectionName}
+        </Link>
+      </h3>
+      <h4>{props.song.primaryGenreName}</h4>
+      <h4>{props.song.releaseDate}</h4>
+    </div>
+  );
+
+  return (
+    <div onClick={() => setView(!view)} style={{ display: "inline-block" }}>
+      <p>{props.title}</p>
+      {view ? detailedView : simpleView}
+    </div>
+  );
 }
 
-export default GalleryItem
+export default GalleryItem;
